2a848231751b285ed3f17428919a3fda0741d73f
aabiilrqm <abilarqam0@gmail.com>
2025-11-08 17:40:16 +0800

    menyelesaikan halaman lupa sandi dan menambahkan halaman enter code

--- app/(auth)/enter_code/page.tsx
+++ app/(auth)/enter_code/page.tsx
@@ -1,14 +1,14 @@
+import { Binary } from "lucide-react";
 import Link from "next/link";
-import { KeyRound } from "lucide-react";
 
-export default function ForgetPasswordPage() {
+export default function EnterCodePage() {
   return (
     <div className="w-5/6 h-[97vh] flex flex-col justify-between  py-3">
       <div>
         <div className="mx-auto mt-2 border w-fit p-2 rounded-xl">
-          <KeyRound />
+          <Binary />
         </div>
-        <h1 className="text-3xl font-semibold text-center">Forget Password?</h1>
+        <h1 className="text-3xl font-semibold text-center">Masukkan kode</h1>
 
         <p className="p-1 mt-2 text-center w-3/4 mx-auto font-light">
           Don’t worry, your account is secure. A verification code will be sent
@@ -17,17 +17,16 @@ export default function ForgetPasswordPage() {
 
         {/* Login Section */}
         <div className="flex flex-col items-center mt-5 gap-2">
-          <div className="w-3/4 flex flex-col gap-1">
-            <h3>Email</h3>
-
-            <input
-              className="w-full bg-gray-100 p-3 rounded-xl font-light text-sm"
-              placeholder="Email"
-            ></input>
+          <div className="w-3/4 flex gap-1 justify-evenly">
+            {Array(6)
+              .fill(0)
+              .map((_, index) => (
+                <div className="p-5 border border-gray-400 rounded-sm" key={index}></div>
+              ))}
           </div>
 
           <div className="py-1 bg-secondary text-white w-1/2 rounded-2xl text-center mt-2">
-            <Link href={'/enter_code'}>Send Code</Link>
+            <p>Enter Code</p>
           </div>
         </div>
       </div>
@@ -38,8 +37,8 @@ export default function ForgetPasswordPage() {
         </p>
 
         <div className="flex justify-center items-center gap-1 mt-2">
-          <span className="h-2 rounded-xl bg-secondary w-full  "></span>
           <span className="h-2 rounded-xl bg-gray-400  w-full  "></span>
+          <span className="h-2 rounded-xl bg-secondary w-full  "></span>
           <span className="h-2 rounded-xl bg-gray-400 w-full  "></span>
         </div>
       </div>
