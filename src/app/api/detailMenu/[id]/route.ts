b80c21c74e306aab27a1435bcdf2a70de1c6f12a
aabiilrqm <abilarqam0@gmail.com>
2025-12-07 09:41:33 +0800

    Memperbaiki error di detailMenu

--- src/app/api/detailMenu/[id]/route.ts
+++ src/app/api/detailMenu/[id]/route.ts
@@ -1,26 +1,19 @@
 import { NextRequest, NextResponse } from "next/server";
 import { retrieveDataById } from "@/src/lib/firebase/service";
 
-export async function GET(req: NextRequest, {params} : {params: {id: string}}) {
-  const {id} = await params
+export async function GET(
+  req: NextRequest,
+  context: { params: { id: string } }
+) {
+  const { id } = context.params; // FIX
 
-  console.log(id)
-  try { 
-      const data = await retrieveDataById("menu", id);
-      return NextResponse.json(data);
-    } catch {
-      return NextResponse.json({
-        status: 500,
-        error: "Failed to fetch data",
-      });
-    }
-
-  // try {
-  //   const data = await retrieveDataById("menu", id!);
-  //   return NextResponse.json(data);
-  // } catch {
-  //   return NextResponse.json({
-  //     error: "Failed to fetch data",
-  //   });
-  // }
+  try {
+    const data = await retrieveDataById("menu", id);
+    return NextResponse.json(data, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Failed to fetch data" },
+      { status: 500 }
+    );
+  }
 }
